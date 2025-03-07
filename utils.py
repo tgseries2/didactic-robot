1dfbe328d7d0ce9618d70aedf7a467e022578cbd
tgseries2 <tgseries.2@gmail.com>
2025-03-08 01:52:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,26 +1,41 @@
 # utils.py
 
 from web3 import Web3
+import requests
 from config import CONFIG
 
-# Initialize Web3 connection
 web3 = Web3(Web3.HTTPProvider(CONFIG["RPC_URL"]))
 
-def get_token_reserves(dex_router, token_a, token_b):
-    """Fetches token reserves from the given DEX router."""
-    router_contract = web3.eth.contract(address=dex_router, abi=UNISWAP_ABI)
-    reserves = router_contract.functions.getReserves(token_a, token_b).call()
-    return reserves
+# Factory & Pair ABIs
+UNISWAP_FACTORY_ABI = '[{"constant":true,"inputs":[],"name":"allPairsLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allPairs","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]'
+PAIR_ABI = '[{"constant":true,"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]'
 
-def calculate_profit(reserve_in, reserve_out, amount_in):
-    """Calculates potential profit from arbitrage trade."""
-    amount_out = (amount_in * reserve_out) / (reserve_in + amount_in)
-    return amount_out - amount_in
+def get_all_pairs(dex_factory):
+    """Fetch all token pairs from a DEX factory."""
+    factory_contract = web3.eth.contract(address=dex_factory, abi=UNISWAP_FACTORY_ABI)
+    total_pairs = factory_contract.functions.allPairsLength().call()
 
-def is_profitable_trade(token_a, token_b, amount):
-    """Checks if an arbitrage opportunity exists between two DEXes."""
-    uni_reserves = get_token_reserves(CONFIG["UNISWAP_ROUTER"], token_a, token_b)
-    sushi_reserves = get_token_reserves(CONFIG["SUSHISWAP_ROUTER"], token_a, token_b)
+    token_pairs = []
+    for i in range(total_pairs):
+        pair_address = factory_contract.functions.allPairs(i).call()
+        pair_contract = web3.eth.contract(address=pair_address, abi=PAIR_ABI)
 
-    profit = calculate_profit(uni_reserves[0], sushi_reserves[1], amount)
-    return profit > 0
+        token0 = pair_contract.functions.token0().call()
+        token1 = pair_contract.functions.token1().call()
+
+        token_pairs.append((token0, token1))
+    return token_pairs
+
+def send_private_transaction(signed_tx):
+    """Submit a transaction privately via bloXroute/Eden."""
+    private_rpc = CONFIG["PRIVATE_RPC_URL"]
+
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "eth_sendPrivateTransaction",
+        "params": [{"tx": signed_tx.rawTransaction.hex()}],
+        "id": 1
+    }
+
+    response = requests.post(private_rpc, json=payload).json()
+    return response.get("result", "TX Failed")
